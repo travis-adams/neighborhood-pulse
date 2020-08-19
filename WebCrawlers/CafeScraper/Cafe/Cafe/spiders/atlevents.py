afcec8b82e23392e63afbee92fd1d597352271b9
Jordan Goldstein <JordanGoldstein@gatech.edu>
2020-08-19 12:26:08 -0400

    Added Location scraping

--- WebCrawlers/CafeScraper/Cafe/Cafe/spiders/atlevents.py
+++ WebCrawlers/CafeScraper/Cafe/Cafe/spiders/atlevents.py
@@ -12,17 +12,20 @@ class atleventsSpider(scrapy.Spider):
     def parse(self, response):
         names = []
         dates = []
+        locs = []
         descs = []
         event_list = response.css('div.event-list') #Grabs the event list module
         for section in event_list.xpath('.//section'): #For every section in the Event list (Editors pick, etc.)
             for event in section.css('div.secondaryTile'):#For each event tile
                 e = event.css('div.secondaryTileCopy')# The Event tile itself
-                name = e.css('.floatRight').xpath('.//h2/a/text()').get().strip()
+                name = e.css('.floatRight').xpath('.//h2/a/text()').get().strip()#Name
                 names.append(name)
-                date = e.css('.floatRight').css('p.eventDate::text').get().strip()
+                date = e.css('.floatRight').css('p.eventDate::text').get().strip()#Date
                 dates.append(date)
-                desc = e.css('.floatRight').css('p.eventDescription::text').get().strip()
+                desc = e.css('.floatRight').css('p.eventDescription::text').get().strip()#Description
                 descs.append(desc)
+                loc = e.css('.floatRight').css('p.noMargin::text').get().strip()#LocationName
+                locs.append(loc)
         #Cool Connection Stuff
         host = 'database-1.c8futoifja4g.us-east-2.rds.amazonaws.com'
         port = 3306
@@ -32,8 +35,8 @@ class atleventsSpider(scrapy.Spider):
 
         conn = pymysql.connect(host,user=username, port=port, passwd=password, db=dbname) #Connect
         cursor = conn.cursor()#Get a cursor to the data
-        for (name, date, desc) in zip(names,dates,descs): #Iterate through all the lists
-            sql = "INSERT INTO DirtyEvents VALUES(%s, %s, %s)"#Insert
-            cursor.execute(sql,(name,date,desc,))#Executre the insert
+        for (name, date, desc,loc) in zip(names,dates,descs,locs): #Iterate through all the lists
+            sql = "INSERT INTO DirtyEvents VALUES(%s, %s, %s,%s)"#Insert
+            cursor.execute(sql,(name,date,desc,loc,))#Executre the insert
         conn.commit()#Commit all inserts
         conn.close()#Close the connection
