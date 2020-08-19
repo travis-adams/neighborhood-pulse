82df4aa479bc418b5afc5399fc9312c30fef0803
Jordan Goldstein <JordanGoldstein@gatech.edu>
2020-08-19 12:15:12 -0400

    Added Comments

--- WebCrawlers/CafeScraper/Cafe/Cafe/spiders/atlevents.py
+++ WebCrawlers/CafeScraper/Cafe/Cafe/spiders/atlevents.py
@@ -6,34 +6,34 @@ import pymysql
 class atleventsSpider(scrapy.Spider):
     name = "events"
     start_urls = [
-        'https://www.atlanta.net/events/#event'
+        'https://www.atlanta.net/events/#event'#The site to go to
     ]
 
     def parse(self, response):
         names = []
         dates = []
         descs = []
-        event_list = response.css('div.event-list')
-        for section in event_list.xpath('.//section'):
-            for event in section.css('div.secondaryTile'):
-                e = event.css('div.secondaryTileCopy')
+        event_list = response.css('div.event-list') #Grabs the event list module
+        for section in event_list.xpath('.//section'): #For every section in the Event list (Editors pick, etc.)
+            for event in section.css('div.secondaryTile'):#For each event tile
+                e = event.css('div.secondaryTileCopy')# The Event tile itself
                 name = e.css('.floatRight').xpath('.//h2/a/text()').get().strip()
                 names.append(name)
                 date = e.css('.floatRight').css('p.eventDate::text').get().strip()
                 dates.append(date)
                 desc = e.css('.floatRight').css('p.eventDescription::text').get().strip()
                 descs.append(desc)
-
+        #Cool Connection Stuff
         host = 'database-1.c8futoifja4g.us-east-2.rds.amazonaws.com'
         port = 3306
         dbname = 'new_schema'
         username = 'CafeAmbassador'
         password = 'TravisHasABeardNow'
 
-        conn = pymysql.connect(host,user=username, port=port, passwd=password, db=dbname)
-        cursor = conn.cursor()
-        for (name, date, desc) in zip(names,dates,descs):
-            sql = "INSERT INTO new_table VALUES(%s, %s, %s)"
-            cursor.execute(sql,(name,date,desc,))
-        conn.commit()
-        conn.close()
+        conn = pymysql.connect(host,user=username, port=port, passwd=password, db=dbname) #Connect
+        cursor = conn.cursor()#Get a cursor to the data
+        for (name, date, desc) in zip(names,dates,descs): #Iterate through all the lists
+            sql = "INSERT INTO DirtyEvents VALUES(%s, %s, %s)"#Insert
+            cursor.execute(sql,(name,date,desc,))#Executre the insert
+        conn.commit()#Commit all inserts
+        conn.close()#Close the connection
