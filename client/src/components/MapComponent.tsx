fd1972e1b8529eed53a8e76a6a9dd1cf309133ea
Chanelleah <51858445+Chanelleah@users.noreply.github.com>
2020-09-23 18:54:33 -0400

    Added Event Card

--- client/src/components/MapComponent.tsx
+++ client/src/components/MapComponent.tsx
@@ -21,7 +21,6 @@ const MapComponent: FC = () => {
   // Asynchronously load the events
   const loadEvents = async (numEvents: number) => {
     var eventList = await eventService.fetchEventsWithLimit(numEvents).then((fetchedEvents: IEvent[]) => {
-      console.log(fetchedEvents)
       return fetchedEvents;
     });
     setEvents(eventList);
@@ -43,7 +42,7 @@ const MapComponent: FC = () => {
 
   return (
     <LoadScript
-      googleMapsApiKey="AIzaSyBpNi3qyG9Vhc3zMKZH1ZYnuQLT0AMDzQ4"
+      googleMapsApiKey="API_KEY_HERE"
     >
       <GoogleMap
         id={"map"}
