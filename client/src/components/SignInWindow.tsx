8b7e66e2db4ed2eeb41d9b8626da8487e2fc71a4
Chanelleah <51858445+Chanelleah@users.noreply.github.com>
2020-11-19 20:58:12 -0500

    Frontend - Added Fields to Sign-Up Window

--- client/src/components/SignInWindow.tsx
+++ client/src/components/SignInWindow.tsx
@@ -5,6 +5,11 @@ import { Close } from '@material-ui/icons';
 import EventService from "../service/EventService";
 import useStyles from '../css';
 import User from '../domain/User';
+import Grid from '@material-ui/core/Grid/Grid';
+import Select from '@material-ui/core/Select/Select';
+import InputLabel from '@material-ui/core/InputLabel/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem/MenuItem';
+import FormControl from '@material-ui/core/FormControl/FormControl';
 
 interface Props {
   isSignedIn: boolean;
@@ -22,8 +27,14 @@ interface Props {
 interface SignInFields {
   username: string;
   password: string;
-  usernameValid: boolean;
-  passwordValid: boolean;
+  firstName: string;
+  lastName: string;
+  confirmPassword: string;
+  usernameError: boolean;
+  passwordError: boolean;
+  firstNameError: boolean;
+  lastNameError: boolean;
+  confirmPasswordError: boolean;
 }
 
 const eventService = new EventService();
@@ -32,17 +43,15 @@ const SignInWindow: FunctionComponent<Props> = (props: Props) => {
   const logo = "c1-logo-full.png";
   const classes = useStyles();
   const [errorMessage, setErrorMessage] = useState<string>("");
-  const [signInFields, setSignInFields] = useState<SignInFields>({username: "", password: "", usernameValid: true, passwordValid: true});
+  const [signInFields, setSignInFields] = useState<SignInFields>({username: "", password: "", usernameError: false, passwordError: false, firstName: "", lastName: "", confirmPassword: "", firstNameError: false, lastNameError: false, confirmPasswordError: false});
+  const [group, setGroup] = useState<string>("");
 
-  const handleCloseToast = (event?: React.SyntheticEvent, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    props.setIsToastOpen(false);
-  }
+  const handleGroupChange = (event: React.ChangeEvent<HTMLInputElement>, value: string) => {
+    setGroup(event.target.value);
+  };
 
   const resetSignInFields = () => {
-    setSignInFields({ username: "", password: "", usernameValid: true, passwordValid: true });
+    setSignInFields({ username: "", password: "", usernameError: false, passwordError: false, firstName: "", lastName: "", confirmPassword: "", firstNameError: false, lastNameError: false, confirmPasswordError: false});
   }
 
   const handleCloseSignIn = () => {
@@ -52,20 +61,38 @@ const SignInWindow: FunctionComponent<Props> = (props: Props) => {
   }
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSignInFields({ ...signInFields, username: event.target.value, usernameValid: true});
+    setSignInFields({ ...signInFields, username: event.target.value, usernameError: false});
   }
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSignInFields({ ...signInFields, password: event.target.value, passwordValid: true});
+    setSignInFields({ ...signInFields, password: event.target.value, passwordError: false});
+  }
+
+  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInFields({ ...signInFields, confirmPassword: event.target.value, confirmPasswordError: false});
+  }
+
+  const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInFields({ ...signInFields, firstName: event.target.value, firstNameError: false});
+  }
+
+  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInFields({ ...signInFields, lastName: event.target.value, lastNameError: false});
   }
 
   // Handles signing the user in/up
-  const handleUserSignIn = async (username: string, password: string) => {
+  const handleUserSignIn = async (username: string, password: string, firstName: string, lastName: string, confirmPassword: string) => {
     try {
       if (username.length === 0 || password.length === 0) {
         throw new Error("Username and password required.")
       }
       if (props.isSignUp) {
+        if (firstName.length === 0 || lastName.length === 0) {
+          throw new Error("First and last name required.")
+        } 
+        if (password != confirmPassword) {
+          throw new Error("Passwords do not match.")
+        }
         await eventService.userSignUp(username, password);
       }
       // Hit the backend to sign the user in, then set token and user
@@ -77,8 +104,8 @@ const SignInWindow: FunctionComponent<Props> = (props: Props) => {
       handleCloseSignIn();
       // Display confirmation toast
       props.setIsToastOpen(true);
-    } catch (error) {
-      setSignInFields({ username: "", password: "", usernameValid: false, passwordValid: false })
+    } catch(error) {
+      setSignInFields({ username: "", password: "", usernameError: true, passwordError: true, firstName: "", lastName: "", confirmPassword: "", firstNameError: true, lastNameError: true, confirmPasswordError: true})
       setErrorMessage(error.message);
     }
   }
@@ -91,58 +118,101 @@ const SignInWindow: FunctionComponent<Props> = (props: Props) => {
   }
 
   return (
-    <div>
-      <Dialog open={props.isSignInOpen} onClose={handleCloseSignIn}>
-        <div style={{display: "flex", alignItems: "flex-start"}}>
-          <div className={classes.gap}/>
-          <img src={logo} style={{marginTop: "3%"}} className={classes.logoImg}/>
-          <div className={classes.endDiv}><IconButton onClick={handleCloseSignIn}><Close/></IconButton></div>
-        </div>
-        <DialogContent style={{display: "flex", flexDirection: "column"}}>
-          <TextField
-          autoFocus
-          id="username"
-          label="Username"
-          value={signInFields.username}
-          onChange={handleUsernameChange}
-          error={!signInFields.usernameValid}
-          />
-          <div style={{marginTop: "1%"}} />
-          <TextField
-          id="password"
-          label="Password"
-          type="password"
-          value={signInFields.password}
-          onChange={handlePasswordChange}
-          error={!signInFields.passwordValid}
-          />
-        </DialogContent>
-        <DialogActions disableSpacing style={{display: "flex", flexDirection: "column"}}>
-          <Button
-            onClick={async () => {await handleUserSignIn(signInFields.username, signInFields.password);}}
-            variant="contained"
-            color="primary"
-            size="large">
-            {props.isSignUp ? "Sign Up" : "Sign In"}
-          </Button>
-          <div style={{marginTop: 8}}/>
-          <Button onClick={handleToggleSignUp} color="primary" size="small">
-            {props.isSignUp ? "Return to sign in" : "Create an account"}
-          </Button>
-        </DialogActions>
-        {errorMessage && <Alert severity="error" variant="filled">{errorMessage}</Alert>}
-      </Dialog>
-      <Snackbar
-        open={props.isToastOpen}
-        autoHideDuration={3000}
-        onClose={handleCloseToast}
-        anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+    <Dialog open={props.isSignInOpen} onClose={handleCloseSignIn}>
+      <div style={{display: "flex", alignItems: "flex-start"}}>
+        <div className={classes.gap}/>
+        <img src={logo} style={{marginTop: "3%"}} className={classes.logoImg}/>
+        <div className={classes.endDiv}><IconButton onClick={handleCloseSignIn}><Close/></IconButton></div>
+      </div>
+      <DialogContent style={{display: "flex", flexDirection: "column"}}>
+      <Grid
+        container
+        direction="row"
+        alignContent="space-between"
       >
-        <Alert onClose={handleCloseToast} severity="success">
-          {props.isSignedIn ? "Signed in" : "Signed out"}
-        </Alert>
-      </Snackbar>
-    </div>
+        {props.isSignUp && (
+        <TextField
+        autoFocus
+        id="firstName"
+        label="First Name"
+        style={{width: 275}}
+        value={signInFields.firstName}
+        onChange={handleFirstNameChange}
+        error={signInFields.usernameError}
+        />)}
+        {props.isSignUp && (
+        <TextField
+        autoFocus
+        id="lastName"
+        label="Last Name"
+        style={{width: 275}}
+        value={signInFields.lastName}
+        onChange={handleLastNameChange}
+        error={signInFields.usernameError}
+        />)}
+      </Grid>
+        <TextField
+        autoFocus
+        id="username"
+        label="Username"
+        value={signInFields.username}
+        onChange={handleUsernameChange}
+        error={signInFields.usernameError}
+        />
+        <div style={{marginTop: "1%"}} />
+        <TextField
+        id="password"
+        label="Password"
+        type="password"
+        value={signInFields.password}
+        onChange={handlePasswordChange}
+        error={signInFields.passwordError}
+        />
+        {props.isSignUp && (
+        <TextField
+        autoFocus
+        id="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        value={signInFields.confirmPassword}
+        onChange={handleConfirmPasswordChange}
+        error={signInFields.passwordError}
+        />)}
+      </DialogContent>
+
+      <DialogActions disableSpacing style={{display: "flex", flexDirection: "column"}}>
+        {props.isSignUp && (
+        <FormControl variant="filled" size="small" fullWidth style={{maxWidth: '96%'}}>
+          <InputLabel id="group">Select a Group</InputLabel>
+          <Select
+            variant="filled"
+            id="group"
+            value={group}
+            onChange={handleGroupChange}
+          >
+            <MenuItem value="Group 1">Group 1</MenuItem>
+            <MenuItem value="Group 2">Group 2</MenuItem>
+            <MenuItem value="Group 3">Group 3</MenuItem>
+            <MenuItem value="Group 4">Group 4</MenuItem>
+            <MenuItem value="Group 5">Group 5</MenuItem>
+          </Select>
+        </FormControl>
+        )}
+        <div style={{marginTop: "1%"}} />
+        <Button
+          onClick={async () => {await handleUserSignIn(signInFields.username, signInFields.password, signInFields.firstName, signInFields.lastName, signInFields.confirmPassword);}}
+          variant="contained"
+          color="primary"
+          size="large">
+          {props.isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+        <div style={{marginTop: 8}}/>
+        <Button onClick={handleToggleSignUp} color="primary" size="small">
+          {props.isSignUp ? "Return to sign in" : "Create an account"}
+        </Button>
+      </DialogActions>
+      {errorMessage && <Alert severity="error" variant="filled">{errorMessage}</Alert>}
+    </Dialog>
   )
 }
 
